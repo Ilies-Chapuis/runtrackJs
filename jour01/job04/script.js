a7b5746374a8ae8886cbf8ef07153f23e035f64f
Ilies-Chapuis <ilies.chapuis@laplateforme.io>
2026-04-21 10:41:12 +0200

    Day1, Job01 to Job05 done

---
+++ jour01/job04/script.js
@@ -0,0 +1,12 @@
+function bisextile(année) {
+    let result;
+    if (année = true) {
+        result = "positive";
+    }
+    else {
+        result = "negative";
+    }
+    return result;
+}
+
+console.log(bisextile(2000))
---
+++ jour01/job05/script.js
@@ -0,0 +1,8 @@
+function afficherJoursSemaines() {
+    const fruits = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"];
+    for (i of fruits) {
+        console.log(i)
+    }
+}
+
+afficherJoursSemaines()
